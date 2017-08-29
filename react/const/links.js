bbec30c879fe8d8a1df15cc01c3204b4c4801ab7
Esteban Bellegarde <esteban.bellegarde@gmail.com>
2017-08-28 22:07:42 -0300

    fix bug prod

--- react/const/links.js
+++ react/const/links.js
@@ -1,4 +1,4 @@
-var host = "31.220.58.84"
+var host = process.env.NODE_ENV === prod ? "www.sinreceta.com.ar" : "31.220.58.84"
 var mdfURL="http://"+host+":3001/download/sin-receta-momento-de-furia.zip"
 var notURL = "http://"+host+":3001/download/sin-receta-noticia-oficial.zip"
 var urls={
